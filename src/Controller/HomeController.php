6fc1b9b69c705e33daba3b93e4e2be111b913f02
banon <j.banon01@gmail.com>
2022-06-07 20:15:45 +0200

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
